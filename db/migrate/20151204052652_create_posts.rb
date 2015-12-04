210356082b067d05a6595e89f3a819957c474822
fathergoose <al@ilseman.com>
2015-12-03 23:39:36 -0600

    generate syntax was screwy removed commas

--- db/migrate/20151204052652_create_posts.rb
+++ db/migrate/20151204052652_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string, :title
-      t.text, :body
+      t.string :title
+      t.text :body
       t.string :author
 
       t.timestamps null: false
