efb0c245f96ada9a8576fd7056412548e477e5ce
fathergoose <al@ilseman.com>
2015-12-04 21:36:27 -0600

    fixed _nav_bar issue and added admin auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+  before_action :authenticate_admin!, only: [:admin]
   def welcome
   end
+  def admin
+
+  end
+
+  private
+
+  def authenticate_admin!
+      unless authenticate_user! && current_user.admin
+        redirect_to root_path
+      end
+    end
+    
 end
