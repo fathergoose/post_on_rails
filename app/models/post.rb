6fae19cc16803151374b56c3713196f3f5e72041
fathergoose <al@ilseman.com>
2015-12-04 23:16:43 -0600

    preview method limits body length for index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
-end
+  def preview
+    body[0..139].gsub(/\s\w+\s*$/,'...') # give 140 char preview and replace 
+  end                                    # up to next to last whitespace with ...
+end  
