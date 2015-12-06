efeb123479fd502eefc1c85f5601645dcc834289
fathergoose <al@ilseman.com>
2015-12-05 20:05:44 -0600

    changed preview_post to preview_body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  def preview_post
-    body[0..139].gsub(/\s\w+\s*$/,'...') # give the first chars and carefully append with ...
+  def preview_body
+    body[0..239].gsub(/\s\w+\s*$/,'...') # give the first chars and carefully append with ...
   end 
 end  
