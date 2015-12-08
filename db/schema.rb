78ec0d7cf2f70ff9539be40165d4316456acee92
fathergoose <al@ilseman.com>
2015-12-08 12:00:53 -0600

    + source to images and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208152444) do
+ActiveRecord::Schema.define(version: 20151208154145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151208152444) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "source"
   end
 
   create_table "posts", force: :cascade do |t|
