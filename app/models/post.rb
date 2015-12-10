cde99acb9fb4601fa91e67b01d4c63884b21b5e0
fathergoose <al@ilseman.com>
2015-12-09 23:50:47 -0600

    wiring up a published method for the sake of timestamps

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
   def preview_body
     body[0..239].gsub(/\s\w+\s*$/,'...') # give the first chars and carefully append with ...
   end 
+
+  def publishd_timestamp!
+    time = Time.now 
+    update( published_at: time )
+  end
+  def date_published
+    created_at.strftime("%b %d, %Y")
+  end
+
 end  
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208154145) do
+ActiveRecord::Schema.define(version: 20151210053850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 20151208154145) do
     t.string   "title"
     t.text     "body"
     t.string   "author"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "published",  default: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "published",    default: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
