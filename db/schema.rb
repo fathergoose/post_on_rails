32719721d445f99f3f69fd50c67cb586b2d9d5ef
fathergoose <al@ilseman.com>
2015-12-11 16:24:24 -0600

    added pretext column to posts table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210053850) do
+ActiveRecord::Schema.define(version: 20151211221852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151210053850) do
     t.datetime "updated_at",                   null: false
     t.boolean  "published",    default: false
     t.datetime "published_at"
+    t.text     "pretext"
   end
 
   create_table "users", force: :cascade do |t|
