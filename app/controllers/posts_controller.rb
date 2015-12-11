3627889dde7302ae2370fcf8819edc4d87c0dbbc
fathergoose <al@ilseman.com>
2015-12-11 08:51:01 -0600

    index only includes published posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where published: true
   end
 
   # GET /posts/1
