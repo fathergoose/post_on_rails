f32a116ad83d804e2ff9eaa79d23c94b65d96bf3
fathergoose <al@ilseman.com>
2015-12-11 19:41:28 -0600

    posts have functional pre-text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,7 +75,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :published)
+      params.require(:post).permit(:title, :pretext, :body, :published)
     end
 
     # fathergoose writing this, not devise
