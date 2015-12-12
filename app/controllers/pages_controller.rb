3569350f23fd1e1e23f3c2ca90444cc2bbd40ac1
fathergoose <al@ilseman.com>
2015-12-11 20:26:29 -0600

    front page exists and all links are wrapped

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   before_action :authenticate_admin!, only: [:admin]
   def welcome
+    @post = Post.where(published: true).order(created_at: :desc).limit(1).first
   end
   def admin
     @posts = Post.all
