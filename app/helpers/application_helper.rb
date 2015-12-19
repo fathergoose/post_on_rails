14205c18f11483473e7c86c88a5ad4e0ccc9f21d
fathergoose <al@ilseman.com>
2015-12-18 20:25:34 -0600

    coderay is styling and using github theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,21 @@
 module ApplicationHelper
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.highlight(code, language)
+      CodeRay.scan(code, language).div(:line_numbers => :table)
     end
   end
 
   def markdown(text)
     coderayified = CodeRayify.new(:filter_html => true, 
-                                  :hard_wrap => true)
+                                  :hard_wrap => true,
+                                  :line_numbers => :table)
     options = {
       :fenced_code_blocks => true,
       :no_intra_emphasis => true,
       :autolink => true,
+      :strikethrough => true,
       :lax_html_blocks => true,
+      :superscript => true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
