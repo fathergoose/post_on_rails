aca763b0328e12dbba0a271652bc9851aa565052
fathergoose <al@ilseman.com>
2016-03-11 20:43:58 -0600

    referencing vanilla semantic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-// Loads all Semantic javascripts
-//= require semantic-ui
+//= require semantic  
 //= require script
 //= require_tree .
