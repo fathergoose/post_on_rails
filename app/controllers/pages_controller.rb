cde4a1ac11d674bb54ce3c454661b3281894b047
fathergoose <al@ilseman.com>
2016-06-16 17:29:47 -0500

    improved styling, only whitespace changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
 class PagesController < ApplicationController
   before_action :authenticate_admin!, only: [:admin]
+
   def welcome
-    @post = Post.where(published: true).order(created_at: :desc).limit(1).first
+    @post = Post.where(published: true).order(created_at: :desc).limit(2).first
   end
+
   def admin
     @posts = Post.all
-
   end
 
   private
 
   def authenticate_admin!
-      unless authenticate_user! && current_user.admin
-        redirect_to root_path
-      end
+    unless authenticate_user! && current_user.admin
+      redirect_to root_path
     end
-    
+  end
 end
