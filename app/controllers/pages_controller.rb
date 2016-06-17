d3b40c101a00067f50465bcd9a90e3a35724ba6f
fathergoose <al@ilseman.com>
2016-06-17 11:54:38 -0500

    a more appropriate use of devise helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
   private
 
   def authenticate_admin!
-    unless authenticate_user! && current_user.admin
+    unless user_signed_in? && current_user.admin
       redirect_to root_path
     end
   end
